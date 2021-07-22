bbc07eb2eda525d0fc6f584c2cc1d86e7798b492
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-07-22 08:48:06 -0400

    added div and functions

--- public/script.js
+++ public/script.js
@@ -1,26 +1,31 @@
 const department = document.querySelector('#department')
 const levelOfWear = document.querySelector('#level-of-wear')
 const price = document.querySelector('#price')
+const GSPrice = document.querySelector('#GS-price')
 let percentage = 1
 
-document.querySelector('#calculate').addEventListener('click', () => {
-  
-  if (levelOfWear == 'near-new') {
-    percentage = .3
-  } else if (levelOfWear == 'lightly-used') {
-    percentage = .4
-  } else if (levelOfWear == 'moderately-used') {
-    percentage = .5
-  } else if (levelOfWear == 'heavily-used') {
-    percentage = .6
+function calculatePrecentage(){
+
+  if (levelOfWear.value == 'near-new') {
+    return percentage = .7
+  } else if (levelOfWear.value == 'lightly-used') {
+    return percentage = .6
+  } else if (levelOfWear.value == 'moderately-used') {
+    return percentage = .5
+  } else if (levelOfWear.value == 'heavily-used') {
+    return percentage = .4
   }
+}
+
+function calculatePrice(){
+  calculatePrecentage()
 
   if (department.value == "mens-footwear" || "womens-footwear") {
-    console.log(Math.floor(Number(price.value) * percentage) + '.83')
+    GSPrice.textContent = Math.floor(Number(price.value) * percentage) + '.83'
   } else {
-    console.log(Math.floor(Number(price.value) * percentage) + '.83')
+    GSPrice.textContent = Math.floor(Number(price.value) * percentage) + '.83'
   }
 
-  console.log(percentage)
+}
 
-})
+document.querySelector('#calculate').addEventListener('click', calculatePrice)
