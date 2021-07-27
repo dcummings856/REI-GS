31265ce47e1c879d578cc765b389427a5ea62c75
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-07-27 19:03:52 -0400

    working custom template

--- public/js/custom-script.js
+++ public/js/custom-script.js
@@ -1,5 +1,33 @@
 const department = document.querySelector('#department')
 const customPercentage = document.querySelector('#custom-percentage')
-const price = document.querySelector('#price')
-const GSPrice = document.querySelector('#GS-price')
+const customPrice = document.querySelector('#custom-price')
+const customGSPrice = document.querySelector('#custom-GS-price')
 let percentage = 1
+
+
+function getCustomPercentage(){
+
+  if (customPercentage.value == '30') {
+    percentage = .7
+  } else if (customPercentage.value == '40') {
+    percentage = .6
+  } else if (customPercentage.value == '50') {
+    percentage = .5
+  } else if (customPercentage.value == '60') {
+    percentage = .4
+  } else if (customPercentage.value == '70') {
+    percentage = .3
+  } else if (customPercentage.value == '80') {
+    percentage = .2
+  }
+
+}
+
+function getCustomPrice(){
+  getCustomPercentage()
+
+  return customGSPrice.textContent = Math.floor(Number(customPrice.value) * percentage) + .83
+
+}
+
+document.querySelector('#custom-calculate').addEventListener('click', getCustomPrice)
