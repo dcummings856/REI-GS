0efc154920794e51be4aa1c175430d6499271940
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-09-02 10:06:15 -0400

    removed details route on server

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ app.get('/custom', (req, res) => {
   res.render('custom.ejs')
 })
 
-app.get('/details', (req, res) => {
-  res.render('details.ejs')
-})
-
-
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Listening on localhost:${PORT}`)
 })
